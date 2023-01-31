f599fa52f62046826b03f70b57c1d00a53d17cc7
ineshdeez <122534302+ineshdeez@users.noreply.github.com>
2023-01-31 13:48:02 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,37 @@
+radio.onReceivedNumber(function (receivedNumber) {
+	
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # # . . #
+        # # . # .
+        . . # . .
+        # # . # .
+        # # . . #
+        `)
+    radio.sendNumber(111)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    radio.sendNumber(333)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        # # # # #
+        # # # # #
+        . # # # .
+        `)
+    radio.sendNumber(222)
+})
+radio.setGroup(111)
 basic.forever(function () {
 	
 })
